cdc68946fb0a829679ff05dfb7080737d2d78700
patrikduch <duchpatrik@icloud.com>
2024-03-15 05:44:23 +0100

    DjangoRest integration

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Item
+from .serializers import ItemSerializer
+
+# Create your views here.
+class ItemList(APIView):
+    def get(self, request, format=None):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
